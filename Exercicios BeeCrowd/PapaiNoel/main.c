960e4629c9448d88a50f5731b8db98d23dbbb94a
Andriel Mark da Silva Pinto <165671672+andrielmark@users.noreply.github.com>
2024-11-04 15:43:58 -0300

    .

---
+++ Exercicios BeeCrowd/PapaiNoel/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.lib>
+
+int main{
+    int ponto;
+    char str[100];
+    char[0] = ATR
+    str
+    scanf("%4s",str );
+    scanf("[^\n]",str);
+    while(str != "0"){
+        if(){
+            char direçao
+        }
+    }
+    scanf("%d", &ponto);
+    
+}
